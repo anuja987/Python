2f35e165417d5ba8e0466c9243ec0fe79ece7f60
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2016-05-09 23:29:19 -0700

    Updated file with comments and try-except

--- Socket1.py
+++ Socket1.py
@@ -1,13 +1,32 @@
 import socket
+import sys
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com', 80))
-mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+def socket1():
+    """
+    This program retrieves a document using http protocol to examine the HTTP Response headers
+        Arg: None
+        Returns: None
+        
+    """
+    try:
+        mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mysock.connect(('www.py4inf.com', 80))
+        mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
 
-while True:
-    data = mysock.recv(512)
-    if ( len(data) < 1 ) :
-        break
-    print data;
+        while True:
+            data = mysock.recv(512)
+            if ( len(data) < 1 ) :
+                break
+            print data;
 
-mysock.close()
+        mysock.close()
+    
+    except:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
+    
+if __name__ == "__main__":
+    """
+        main function, starting point of program.
+    """
+    socket1()
