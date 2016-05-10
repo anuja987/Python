1414ea60ea87ce69b5a7d02c7692464d88ca0bfa
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2016-05-09 19:13:57 -0700

    Updated file with comments and try-except

--- tracks.py
+++ tracks.py
@@ -1,100 +1,121 @@
 import xml.etree.ElementTree as ET
 import sqlite3
+import sys
 
-conn = sqlite3.connect('trackdb.sqlite')
-cur = conn.cursor()
+def tracks():
+    """
+        This program creats trackdb.sqlite file from Library.xml.
+       
+        Arg:  None
+        Returns: None
+        
+    """
+    try:
+        conn = sqlite3.connect('trackdb.sqlite')
+        cur = conn.cursor()
 
-# Make some fresh tables using executescript()
-cur.executescript('''
-DROP TABLE IF EXISTS Artist;
-DROP TABLE IF EXISTS Album;
-DROP TABLE IF EXISTS Track;
-DROP TABLE IF EXISTS Genre;
+        # Make some fresh tables using executescript()
+        cur.executescript('''
+        DROP TABLE IF EXISTS Artist;
+        DROP TABLE IF EXISTS Album;
+        DROP TABLE IF EXISTS Track;
+        DROP TABLE IF EXISTS Genre;
 
-CREATE TABLE Artist (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name    TEXT UNIQUE
-);
+        CREATE TABLE Artist (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            name    TEXT UNIQUE
+        );
 
-CREATE TABLE Genre (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name   TEXT UNIQUE
-);
+        CREATE TABLE Genre (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            name   TEXT UNIQUE
+        );
 
-CREATE TABLE Album (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    artist_id  INTEGER,
-    title   TEXT UNIQUE
-);
+        CREATE TABLE Album (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            artist_id  INTEGER,
+            title   TEXT UNIQUE
+        );
 
-CREATE TABLE Track (
-    id  INTEGER NOT NULL PRIMARY KEY
-        AUTOINCREMENT UNIQUE,
-    title TEXT  UNIQUE,
-    album_id  INTEGER,
-    genre_id  INTEGER,
-    len INTEGER, rating INTEGER, count INTEGER
-);
-''')
+        CREATE TABLE Track (
+            id  INTEGER NOT NULL PRIMARY KEY
+                AUTOINCREMENT UNIQUE,
+            title TEXT  UNIQUE,
+            album_id  INTEGER,
+            genre_id  INTEGER,
+            len INTEGER, rating INTEGER, count INTEGER
+        );
+        ''')
 
 
-#fname = raw_input('Enter file name: ')
-fname = "C:\\Users\\Anuja\\Desktop\\Learn\\Course4\\tracks\\Library.xml"
-if ( len(fname) < 1 ) : fname = 'Library.xml'
+        #fname = raw_input('Enter file name: ')
+        fname = "C:\\Users\\Anuja\\Desktop\\Learn\\Course4\\tracks\\Library.xml"
+        if ( len(fname) < 1 ) : fname = 'Library.xml'
 
-# <key>Track ID</key><integer>369</integer>
-# <key>Name</key><string>Another One Bites The Dust</string>
-# <key>Artist</key><string>Queen</string>
-def lookup(d, key):
-    found = False
-    for child in d:
-        if found : return child.text
-        if child.tag == 'key' and child.text == key :
-            found = True
-    return None
+        # <key>Track ID</key><integer>369</integer>
+        # <key>Name</key><string>Another One Bites The Dust</string>
+        # <key>Artist</key><string>Queen</string>
+        def lookup(d, key):
+            found = False
+            for child in d:
+                if found : return child.text
+                if child.tag == 'key' and child.text == key :
+                    found = True
+            return None
 
-stuff = ET.parse(fname)
-all = stuff.findall('dict/dict/dict')
-print 'Dict count:', len(all)
-for entry in all:
-    if ( lookup(entry, 'Track ID') is None ) : continue
+        stuff = ET.parse(fname)
+        all = stuff.findall('dict/dict/dict')
+        print 'Dict count:', len(all)
+        for entry in all:
+            if ( lookup(entry, 'Track ID') is None ) : continue
 
-    name = lookup(entry, 'Name')
-    artist = lookup(entry, 'Artist')
-    album = lookup(entry, 'Album')
-    count = lookup(entry, 'Play Count')
-    rating = lookup(entry, 'Rating')
-    length = lookup(entry, 'Total Time')
-    genre = lookup(entry, 'Genre')
+            name = lookup(entry, 'Name')
+            artist = lookup(entry, 'Artist')
+            album = lookup(entry, 'Album')
+            count = lookup(entry, 'Play Count')
+            rating = lookup(entry, 'Rating')
+            length = lookup(entry, 'Total Time')
+            genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None or genre is None :
-        continue
+            if name is None or artist is None or album is None or genre is None :
+                continue
 
-#    print (name+"  :: "+artist+" :: "+album+" :: "+genre)
+        #    print (name+"  :: "+artist+" :: "+album+" :: "+genre)
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name)
-        VALUES ( ? )''', ( artist, ) )
-    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
-    artist_id = cur.fetchone()[0]
+            cur.execute('''INSERT OR IGNORE INTO Artist (name)
+                VALUES ( ? )''', ( artist, ) )
+            cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+            artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
-        VALUES ( ?, ? )''', ( album, artist_id ) )
-    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
-    album_id = cur.fetchone()[0]
+            cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
+                VALUES ( ?, ? )''', ( album, artist_id ) )
+            cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+            album_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Genre (name)
-        VALUES ( ? )''', ( genre, ) )
-    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
-    genre_id = cur.fetchone()[0]
+            cur.execute('''INSERT OR IGNORE INTO Genre (name)
+                VALUES ( ? )''', ( genre, ) )
+            cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+            genre_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, genre_id, len, rating, count)
-        VALUES ( ?, ?, ?,?, ?, ? )''',
-        ( name, album_id, genre_id, length, rating, count, ) )
+            cur.execute('''INSERT OR REPLACE INTO Track
+                (title, album_id, genre_id, len, rating, count)
+                VALUES ( ?, ?, ?,?, ?, ? )''',
+                ( name, album_id, genre_id, length, rating, count, ) )
 
-conn.commit()
+        conn.commit()
 
-for row in cur.execute('SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN Album JOIN Artist ON Track.genre_id = Genre.ID and Track.album_id = Album.id AND Album.artist_id = Artist.id ORDER BY Artist.name,Track.title LIMIT 3'):
-    print row
+        for row in cur.execute('SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN Album JOIN Artist ON Track.genre_id = Genre.ID and Track.album_id = Album.id AND Album.artist_id = Artist.id ORDER BY Artist.name,Track.title LIMIT 3'):
+            print row
 
-cur.close()
+    except:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
+   
+    finally:  
+        cur.close()
+
+if __name__ == "__main__":
+    """
+        main function, starting point of program.
+    """
+    tracks()
