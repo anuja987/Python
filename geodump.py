8f196af3efc7732e7698e617388237be7ceb81b1
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2016-05-09 17:30:14 -0700

    Updated file with comments and try-except

--- geodump.py
+++ geodump.py
@@ -1,39 +1,55 @@
+import sys
 import sqlite3
 import json
 import codecs
 
-conn = sqlite3.connect('C:\\Python27\\geodata.sqlite')
-cur = conn.cursor()
+def geodump():
+    """
+        This program creates a database of addresses of the locations present in where.js file.
+       
+        Arg:  None
+        Returns: None
+        
+    """
+    conn = sqlite3.connect('C:\\Python27\\geodata.sqlite')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Locations')
+    fhand = codecs.open('where.js','w', "utf-8")
+    fhand.write("myData = [\n")
+    count = 0
+    try:
+        for row in cur :
+            data = str(row[1])
+            try: js = json.loads(str(data))
+            except: continue
 
-cur.execute('SELECT * FROM Locations')
-fhand = codecs.open('where.js','w', "utf-8")
-fhand.write("myData = [\n")
-count = 0
-for row in cur :
-    data = str(row[1])
-    try: js = json.loads(str(data))
-    except: continue
+            if not('status' in js and js['status'] == 'OK') : continue
 
-    if not('status' in js and js['status'] == 'OK') : continue
+            lat = js["results"][0]["geometry"]["location"]["lat"]
+            lng = js["results"][0]["geometry"]["location"]["lng"]
+            if lat == 0 or lng == 0 : continue
+            where = js['results'][0]['formatted_address']
+            where = where.replace("'","")
+            try :
+                print where, lat, lng
 
-    lat = js["results"][0]["geometry"]["location"]["lat"]
-    lng = js["results"][0]["geometry"]["location"]["lng"]
-    if lat == 0 or lng == 0 : continue
-    where = js['results'][0]['formatted_address']
-    where = where.replace("'","")
-    try :
-        print where, lat, lng
-
-        count = count + 1
-        if count > 1 : fhand.write(",\n")
-        output = "["+str(lat)+","+str(lng)+", '"+where+"']"
-        fhand.write(output)
+                count = count + 1
+                if count > 1 : fhand.write(",\n")
+                output = "["+str(lat)+","+str(lng)+", '"+where+"']"
+                fhand.write(output)
+            except:
+                continue
     except:
-        continue
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
+   
+    finally:  
+        cur.close()
+        fhand.close()
 
-fhand.write("\n];\n")
-cur.close()
-fhand.close()
-print count, "records written to where.js"
-print "Open where.html to view the data in a browser"
+if __name__ == "__main__":
+    """
+        main function, starting point of program.
+    """
+    geodump()
 
