7f582f0452070d2623f9ed031ee3f46c5d268b93
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2017-01-21 12:23:11 -0800

    Update mergesort.py
    Updated code with try and except

--- mergesort.py
+++ mergesort.py
@@ -7,10 +7,13 @@ def mergeSort(nlist):
         mid = len(nlist)//2  
         lefthalf = nlist[:mid]  
         righthalf = nlist[mid:]  
-  
+        
         mergeSort(lefthalf)  
         mergeSort(righthalf)  
-        i=j=k=0         
+        i=j=k=0    
+        
+        try:
+            
         while i < len(lefthalf) and j < len(righthalf):  
             if lefthalf[i] < righthalf[j]:  
                 nlist[k]=lefthalf[i]  
@@ -19,17 +22,22 @@ def mergeSort(nlist):
                 nlist[k]=righthalf[j]  
                 j=j+1  
             k=k+1  
-  
+
         while i < len(lefthalf):  
             nlist[k]=lefthalf[i]  
             i=i+1  
             k=k+1  
-  
+
         while j < len(righthalf):  
             nlist[k]=righthalf[j]  
             j=j+1  
             k=k+1  
-    print("Merging ",nlist)  
+    print("Merging ",nlist)
+    
+        except:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
+
   
 nlist = [14,46,43,27,57,41,45,21,70]  
 mergeSort(nlist)  
