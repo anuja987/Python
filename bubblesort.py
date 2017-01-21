88ff6a4eedf179a3d6c79b083c85cb42cd0394d2
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2017-01-20 20:14:50 -0800

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -2,7 +2,11 @@ def bubble_sort(myList):
     """The bubble sort makes multiple passes through a list. It compares adjacent items and exchanges those that are out of order.
     Each pass through the list places the next largest value in its proper place. In essence, each item bubbles up to the location
     where it belongs."""
+    
     moreswaps=True
+    
+    try:
+        
     while moreswaps:
         moreswaps=False
         for i in range(len(myList)-1):
@@ -12,6 +16,10 @@ def bubble_sort(myList):
                 myList[i] = myList[i+1]
                 myList[i+1] = temp
     return  myList
+    
+    except:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
 
 myList=[32,21,67,90,29,10,34]
 sortedlist= bubble_sort(myList)
