497ceb3169076aaba3e04391ea212b73bfb171ba
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2017-01-22 15:20:28 -0800

    Update temperatureconverter.py
    Updated program with try and except block

--- temperatureconverter.py
+++ temperatureconverter.py
@@ -4,4 +4,5 @@ try:
     cel = (fahr - 32.0) * 0.5 / 0.9
     print cel
 except:
-    print("Please input a number")
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
