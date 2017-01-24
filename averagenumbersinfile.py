3edbc30bb7ef2721c02aa54882783843cc9f29a9
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2017-01-23 20:26:04 -0800

    Update averagenumbersinfile.py

--- averagenumbersinfile.py
+++ averagenumbersinfile.py
@@ -33,3 +33,9 @@ if __name__ == "__main__":
     except:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
+        
+        
+if __name__ == "__main__":
+    """
+        main function, starting point of program.
+    """
