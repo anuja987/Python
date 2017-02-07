2875de02f73c1860a81f497bcd474e581d87ba04
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2017-02-07 08:49:25 -0800

    Updated file with try except

--- web.py
+++ web.py
@@ -3,7 +3,8 @@ import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('www.pythonlearn.com', 80))
 mysock.send('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
-
+try:
+    
 while True:
     data = mysock.recv(512)
     if ( len(data) < 1 ) :
@@ -11,3 +12,7 @@ while True:
     print data;
 
 mysock.close()
+
+except:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
