8078b27332cc26c2aa3dc04862e2d05ed091ab4a
Anuja Agraharkar <anuja.agraharkar@gmail.com>
2017-03-12 14:52:02 -0700

    Update tupleoperation1.py

--- tupleoperation1.py
+++ tupleoperation1.py
@@ -31,9 +31,9 @@ def tuples():
           print key, val
 
 
-     except:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
+    except:
+         exc_type, exc_obj, exc_tb = sys.exc_info()
+         print ("Exception occured. Type:{0}, Object: {1}, TraceBack: {2}", exc_type, exc_obj, exc_tb)
 
 if __name__ == "__main__":
     """
